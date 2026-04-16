aa6f6004e0e940ac47b082fc5c4c6910414112d2
Olymbias <123255787+Olymbias@users.noreply.github.com>
2026-04-16 11:33:24 +0200

    Page accueil

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+      return (
+          <main>
+                <h1>Convention JDR 2026</h1>
+                      <p>Bienvenue sur la billetterie de la convention !</p>
+                          </main>
+                            )
+                            }
+}
