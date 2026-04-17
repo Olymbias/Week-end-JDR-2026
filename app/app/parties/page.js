240d216d3a91012ff0e834c30ee0d71236a461ed
Olymbias <123255787+Olymbias@users.noreply.github.com>
2026-04-17 10:41:27 +0200

    error

--- app/app/parties/page.js
+++ app/app/parties/page.js
@@ -40,3 +40,4 @@ const supabase = createClient(
                                                                                                                                                     </main>
                                                                                                                                                       )
                                                                                                                                                     
+                                  }
