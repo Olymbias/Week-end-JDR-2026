bad824d5d35a4f6bdb8c75b913363d1ab7637bab
Olymbias <123255787+Olymbias@users.noreply.github.com>
2026-04-19 14:15:53 +0200

    commit

--- app/inscription/page.jsx
+++ app/inscription/page.jsx
@@ -1,6 +1,7 @@
 export const dynamic = 'force-dynamic'
 import InscriptionClient from './InscriptionClient'
 
-export default function Page({ searchParams }) {
-  return <InscriptionClient token={searchParams.token} />
+export default async function Page({ searchParams }) {
+  const params = await searchParams
+  return <InscriptionClient token={params?.token} />
 }
